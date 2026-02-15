c7e725d4e7ef38698200bfa638653bf51353e433
wulfia <mmwulfia@gmail.com>
2026-02-15 14:11:24 +0300

    fix PATH

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_folder='static', 
+            template_folder='templates')
 
 @app.route('/')
 def index():
@@ -12,5 +14,5 @@ def health():
     return {'status': 'ok'}, 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
